ada912729f1ac05df4d108f4c5a03d101aa4a66d
Altynbek2021 <shatmanalie0694@gmail.com>
2024-11-03 18:41:18 +0600

    finished Xylophone app

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,36 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
         home: Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            DifferenSounds(note: 'DO', num: 1, color: Colors.blue),
-            DifferenSounds(note: "RE", num: 2, color: Colors.green),
-            DifferenSounds(note: "ME", num: 3, color: Colors.red),
-            DifferenSounds(note: "FA", num: 4, color: Colors.yellowAccent),
-            DifferenSounds(note: "SOL", num: 5, color: Colors.lightGreen),
-            DifferenSounds(note: "LYA", num: 6, color: Colors.lightBlue),
-            DifferenSounds(note: "SI", num: 7, color: Colors.lightGreenAccent),
-          ],
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                  child:
+                      DifferenSounds(note: 'DO', num: 1, color: Colors.blue)),
+              Expanded(
+                  child:
+                      DifferenSounds(note: "RE", num: 2, color: Colors.green)),
+              Expanded(
+                  child: DifferenSounds(note: "ME", num: 3, color: Colors.red)),
+              Expanded(
+                  child: DifferenSounds(
+                      note: "FA", num: 4, color: Colors.yellowAccent)),
+              Expanded(
+                  child: DifferenSounds(
+                      note: "SOL", num: 5, color: Colors.lightGreen)),
+              Expanded(
+                  child: DifferenSounds(
+                      note: "LYA", num: 6, color: Colors.lightBlue)),
+              Expanded(
+                child: DifferenSounds(
+                    note: "SI", num: 7, color: Colors.lightGreenAccent),
+              ),
+            ],
+          ),
         ),
       ),
     ));
@@ -46,7 +62,8 @@ class DifferenSounds extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 30, top: 10, right: 50),
       child: TextButton(
-          style: TextButton.styleFrom(backgroundColor: color),
+          style: TextButton.styleFrom(
+              backgroundColor: color, minimumSize: Size(200, 50)),
           onPressed: () async {
             final player = AudioPlayer();
             await player.play(AssetSource('assets_note$num.wav'));
